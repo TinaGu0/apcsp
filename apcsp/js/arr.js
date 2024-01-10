a552949475d79e3c91a3c311c7a2ccf321e77fd0
TinaGu0 <tinaguo0904@gmail.com>
2024-01-10 10:15:16 -0500

    a

---
+++ apcsp/js/arr.js
@@ -0,0 +1,25 @@
+const colors = [
+    {
+        color1: "red" , 
+        color2: "green" ,
+        color3: "blue" ,
+        color4: "yellow" , 
+    },
+    {
+        color1: "purple" , 
+        color2: "red" , 
+        color3: "pink" ,
+        color4: "orange" , 
+    },
+    {
+        color1: "gray" , 
+        color2: "brown" , 
+        color3: "black" , 
+        color4: "white" , 
+    },
+];
+
+
+//write an array with all the answers, for the algorithm, compare the answers with what the user clicked. Display wrong answers at the end. 
+
+export {colors}
---
+++ apcsp/js/main.js
@@ -0,0 +1,8 @@
+import "../css/style.css"
+import {colors} from "./arr"
+
+colors.forEach((a) => console.log(a))
+
+function deleteStartingPage(){
+  console.log("im clickeD!")
+}
