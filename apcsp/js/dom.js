e495e27fcc7cea354e5f0502401176fb17ca2882
TinaGu0 <tinaguo0904@gmail.com>
2024-01-12 10:13:08 -0500

    aadadadad

--- apcsp/js/dom.js
+++ apcsp/js/dom.js
@@ -2,5 +2,6 @@ const DOM = {
     startGame: document.querySelector("#begin"),
     startPage: document.querySelector("#startingPage"),
     questions: document.querySelector("#questions"),
+    submit: document.querySelector("#submitButton")
 };
 export {DOM};
--- apcsp/js/main.js
+++ apcsp/js/main.js
@@ -7,15 +7,33 @@ import {answers} from "./arr"
 DOM.startGame.addEventListener("click", function(){
   DOM.startPage.remove();
   showQuestions(colors);
+  submit();
 });
 
 //questions
 function showQuestions(arr){
   arr.forEach((options) => DOM.questions.insertAdjacentHTML("beforeend", 
   `
-  <div>
-    <p>${options.color1}</p>
-  </div>
+  <section>
+    <div> 
+      <img src="${options.img}" class="image" />
+    </div>
+    <div>
+      <button class="button" id="#button">${options.color1}</button><button class="button">${options.color2}</button><button class="button">${options.color3}</button><button class="button">${options.color4}</button>
+   </div>
+  </section>
   `
   ));
+  getUserAnswers();
 };
+
+function submit(){
+  DOM.submit.insertAdjacentHTML("afterend", `
+  <button class="submitButton">Submit</button>
+  `);
+};
+
+function getUserAnswers(){
+  let buttons = document.querySelectorAll("#button");
+  buttons.forEach((btn)=> console.log(btn.textContent))
+}
